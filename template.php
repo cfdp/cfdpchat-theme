091b1a9961cf25d1c03bf272463618317d3b7301
Michael Lajlev <lajlev@gmail.com>
2013-12-03 11:52:42 +0100

    Disable zoom by setting viewport meta tag

--- template.php
+++ template.php
@@ -4,5 +4,17 @@
  * @file template.php
  */
 function cfdpchat_preprocess_html(&$variables) {
+
+  // Set viewport meta tag - http://www.webomelette.com/drupal-add-html-tag-head-meta-link
+  $viewport = array(
+    '#tag' => 'meta',
+    '#attributes' => array(
+      'name' => 'viewport',
+      'content' => 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+    ),
+  );
+
+  drupal_add_html_head($viewport, 'viewport');
+
 	drupal_add_css('http://fonts.googleapis.com/css?family=Ubuntu', array('type' => 'external'));
 }
